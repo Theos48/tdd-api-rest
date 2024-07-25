3ee39f992bcd6a74c580bdb986c3d33cecd2de4e
Theos48 <david_ramirezz@hotmail.com>
2024-07-25 02:58:14 -0600

    Definicion de rutas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        using: function() {
+            $PREFIX_API = 'api';
+            $PREFIX_API_APP = $PREFIX_API . '/v1';
+            $PREFIX_API_APP_AUTH = $PREFIX_API . '/auth';
+
+            Route::middleware('api')
+                -> prefix($PREFIX_API)
+                -> group(base_path('routes/api.php'));
+
+            Route::middleware('api')
+                -> prefix($PREFIX_API_APP_AUTH)
+                -> group(base_path('routes/api/auth.php'));
+        },
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,14 +3,17 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
+     *
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    #[Test]
+    public function hello_world_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
